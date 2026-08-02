b89ff730e6a27c76a72b0d4c9bdc31007e92a865
Mohamed Ali Ben Belhassen <mohamedalibelhassen@gmail.com>
2026-08-02 21:09:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+document.getElementById('gift').onclick=()=>{
+ document.getElementById('hero').style.display='none';
+ document.getElementById('surprise').classList.remove('hidden');
+ if(navigator.vibrate){navigator.vibrate(40);}
+ window.scrollTo({top:0,behavior:'smooth'});
+};
+
